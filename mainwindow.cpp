966d2b1abdc56ea0c5b112d65cdedb5e294d9abc
Margarita Golubeva <golubeva.rita2013@yandex.ru>
2018-08-12 13:11:45 +0700

    Version 3. Added ability to switch between layers.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,6 +13,7 @@ MainWindow::MainWindow(QWidget *parent) :
     file->addAction(open);
 
     connect(open, SIGNAL(triggered()), this, SLOT(LoadImage()));
+    connect(ui->comboBox_layer, SIGNAL(activated(int)), this, SLOT(SelectLayer(int)));
 }
 
 MainWindow::~MainWindow()
@@ -34,16 +35,33 @@ void MainWindow::LoadImage()
 
 void MainWindow::ConstructImagePyramid()
 {
-    imageSizes.resize(2);
+    int i = 0;
 
     int w = (*pix).width();
     int h = (*pix).height();
 
+    imageSizes.clear();
+    imageSizes.resize(2);
+
+    ui->comboBox_layer->clear();
     while (w>=1 && h>=1)
     {
         imageSizes[0].push_back(w);
         imageSizes[1].push_back(h);
         w/=2;
         h/=2;
+
+        ui->comboBox_layer->addItem(QString::number(i));
+        i++;
     }
+}
+
+void MainWindow::SelectLayer(int index)
+{
+    QPixmap map=*pix;
+    map=map.scaled(imageSizes[0][index], imageSizes[1][index]);
+    ui->label_image->setPixmap(map.scaled(imageSizes[0][0], imageSizes[1][0]));
+    ui->scrollArea_image->setWidget(ui->label_image);
+
+    ui->label_size->setText("Size: " + QString::number(imageSizes[0][index]) + "x" + QString::number(imageSizes[1][index]));
 }
